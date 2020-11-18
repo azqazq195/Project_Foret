bd21fe11519d19806cb113e39f4f6744ed6aa3e0
문성하 <azqazq195@gmail.com>
2020-11-18 18:23:08 +0900

    20201118 feature/androidLogin 작성 중 저장

--- sql/foret_member.sql
+++ sql/foret_member.sql
@@ -12,9 +12,18 @@ member_photo   varchar2(   500   )
 
 select * from member_table;
 
-create sequence member_no nocache nocycle;
+create sequence	seq_member_id	nocache nocycle;	
 
-insert into member_table value (member_no.nextval, '이메일', '닉네임', );
-insert into freeboard_table values (freeboard_no.nextval, 0, 0, 0, #{member_id}, #{freeboard_subject}, #{freeboard_content}, sysdate, sysdate)
+insert into member_table values (seq_member_id.nextval, '이름', '닉네임', '이메일', '19000000', sysdate, '구글', '서울시 강남구', 'url');
+
+update member_table set 
+member_name = '이름 수정',
+member_nickname = '닉네임 수정',
+member_email = '이메일 수정',
+member_birth = '생일 수정',
+member_chain = '연동 수정',
+member_address = '주소 수정',
+member_photo = '사진 수정' 
+where member_id=1;
 
 commit;
