0dba2175bef86867f235d0ccf7ef6f846b0f208c
moseoh <45132207+azqazq195@users.noreply.github.com>
2020-12-24 20:16:53 +0900

    Create Spring Boot .gradle

---
+++ springboot/foret_gradle/foret/src/test/java/com/foret/foret/ForetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foret.foret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
